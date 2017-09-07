edc460cb55772438711fa2fb6ca54c56aec46b81
Student <student@ent-v314-013.local>
2017-09-07 14:31:19 -0400

    syncing folder

---
+++ Week 1/Assets/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	// Vector 3 is a variable type that stores x, y and z info
+	// we have to name the variable 
+	public Vector3 my_Rotation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate ( my_Rotation * Time.deltaTime);
+	}
+}
