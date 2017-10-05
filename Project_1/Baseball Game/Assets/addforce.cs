48a4d009e9e6abb159cbb36dd2b1998206033033
dvalle328 <student@ent-v314-013.local>
2017-10-05 16:01:53 -0400

    Baseball Project

---
+++ Project_1/Baseball Game/Assets/addforce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addforce : MonoBehaviour {
+
+
+	public Vector3 extrabounce;
+	public float multiplier;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter (Collision col){
+
+		col.gameObject.GetComponent<Rigidbody> ().AddForce (extrabounce * multiplier);
+
+	}
+
+
+}
---
+++ Project_1/Baseball Game/readme.md
@@ -0,0 +1,7 @@
+# Project 1: Donovan Valle
+
+Write your description
+
+And possibly some instructions for use
+
+![the project in action](doc.proj1.gif)
